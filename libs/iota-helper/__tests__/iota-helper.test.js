730ee28334c7bb034a16b72aef21eb5d28250ba8
Daniele Piscaglia <danielepiscaglia@libero.it>
2021-02-12 12:04:46 +0100

    Seed generation

--- libs/iota-helper/__tests__/iota-helper.test.js
+++ libs/iota-helper/__tests__/iota-helper.test.js
@@ -1,7 +1,20 @@
 'use strict';
 
+const { expect } = require('chai');
 const iotaHelper = require('..');
 
 describe('@iota-supply-tracer/iota-helper', () => {
-    it('needs tests');
+    describe('generateSeed', () => {
+        it('should generate seed', () => {
+            const seed = iotaHelper.generateSeed();
+            expect(seed).to.have.lengthOf(81);
+            expect(seed.includes('0')).to.be.false;
+            expect(seed.includes('1')).to.be.false;
+        })
+        it('should generate different seeds', () => {
+            const seed1 = iotaHelper.generateSeed();
+            const seed2 = iotaHelper.generateSeed();
+            expect(seed1).to.be.not.equal(seed2);
+        })
+    })
 });
--- libs/iota-helper/lib/iota-helper.js
+++ libs/iota-helper/lib/iota-helper.js
@@ -1,7 +1,15 @@
 'use strict';
 
-module.exports = iotaHelper;
-
-function iotaHelper() {
-    // TODO
+const iotaHelper = {
+    generateSeed: () => {
+        var seed = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ9';
+        var charactersLength = characters.length;
+        for (var i = 0; i < 81; i++) {
+            seed += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return seed;
+    }
 }
+
+module.exports = iotaHelper;
